36ab95643adf11e1febdb43b5865aeb09cb6ef4b
Daniil <stankov370@gmail.com>
2024-09-22 13:42:03 +0300

    Completed Task 5

---
+++ Lab_2/Task_5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        const int S = 9; // Розмір матриці 9x9
+        int[,] matrix = new int[S, S]; // Створення квадратної матриці
+        int k = 1; // Лінійна послідовність чисел (ЛПЧ)
+
+        // Заповнення матриці секторами вище та нижче діагоналей ЛПЧ
+        for (int i = 0; i < S; i++)
+        {
+            for (int j = 0; j < S; j++)
+            {
+                if (j > i && j < S - i - 1) // Сектори вище головної та побічної діагоналей
+                {
+                    matrix[i, j] = k++;
+                }
+                else if (j < i && j > S - i - 1) // Сектори нижче головної та побічної діагоналей
+                {
+                    matrix[i, j] = k++;
+                }
+                else
+                {
+                    matrix[i, j] = 0; // Залишок матриці заповнюємо нулями
+                }
+            }
+        }
+
+        // Виведення матриці
+        Console.WriteLine("Матриця:");
+        for (int i = 0; i < S; i++)
+        {
+            for (int j = 0; j < S; j++)
+            {
+                Console.Write($"{matrix[i, j],3} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
