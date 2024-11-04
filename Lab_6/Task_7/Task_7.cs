36cc3a28ae2f623775fabda785d2b50ba61301a3
Daniil Stankov <stankov370@gmail.com>
2024-11-04 02:00:46 +0200

    Task_7 done.

---
+++ Lab_6/Task_7/Task_7.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_7
+{
+    public partial class Task_7 : Form
+    {
+        public Task_7()
+        {
+            InitializeComponent();
+        }
+    }
+}
