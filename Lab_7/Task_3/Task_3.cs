de8c49b88452140eb14b0610b0fb87cd142385af
Daniil Stankov <stankov370@gmail.com>
2024-11-05 01:16:09 +0200

    Task_3 done.

---
+++ Lab_7/Task_3/Task_3.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_3
+{
+    public partial class Task_3 : Form
+    {
+        public Task_3()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_DoubleClick(object sender, EventArgs e)
+        {
+            Label label = sender as Label;
+
+            if (label != null)
+            {
+                if (label.Font.Italic)
+                {
+                    label.Font = new Font(label.Font, FontStyle.Regular);
+                }
+                else
+                {
+                    label.Font = new Font(label.Font, FontStyle.Italic);
+                }
+            }
+        }
+
+        private void label2_DoubleClick(object sender, EventArgs e)
+        {
+            Label label = sender as Label;
+
+            if (label != null)
+            {
+                if (label.Font.Italic)
+                {
+                    label.Font = new Font(label.Font, FontStyle.Regular);
+                }
+                else
+                {
+                    label.Font = new Font(label.Font, FontStyle.Italic);
+                }
+            }
+        }
+
+        private void label3_DoubleClick(object sender, EventArgs e)
+        {
+            Label label = sender as Label;
+
+            if (label != null)
+            {
+                if (label.Font.Italic)
+                {
+                    label.Font = new Font(label.Font, FontStyle.Regular);
+                }
+                else
+                {
+                    label.Font = new Font(label.Font, FontStyle.Italic);
+                }
+            }
+        }
+    }
+}
