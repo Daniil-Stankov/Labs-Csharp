5b931bb9ffed6452b4815335989921a5c18a42d4
Daniil Stankov <stankov370@gmail.com>
2024-11-05 01:04:23 +0200

    Task_1 done.

---
+++ Lab_7/Task_1/Task_1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_7
+{
+    public partial class Task_1 : Form
+    {
+        public Task_1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Ви натиснули кнопку 1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Ви натиснули кнопку 2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Ви натиснули кнопку 3";
+        }
+    }
+}
