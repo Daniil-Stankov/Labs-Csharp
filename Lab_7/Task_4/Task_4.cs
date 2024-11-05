35ca26148531ccd2d554a33f5e49c394b8c3cf2a
Daniil Stankov <stankov370@gmail.com>
2024-11-05 10:54:28 +0200

    Task_4 done.

---
+++ Lab_7/Task_4/Task_4.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_4
+{
+    public partial class Task_4 : Form
+    {
+        public Task_4()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void label_DoubleClick(object sender, EventArgs e)
+        {
+            // Отримуємо елемент, на якому сталося подія
+            Label label = sender as Label;
+
+            if (label != null)
+            {
+                // Перевіряємо, чи шрифт зараз у стилі курсиву
+                if (label.Font.Italic)
+                {
+                    // Якщо шрифт курсивний, змінюємо його на звичайний
+                    label.Font = new Font(label.Font, FontStyle.Regular);
+                }
+                else
+                {
+                    // Якщо шрифт звичайний, змінюємо на курсив
+                    label.Font = new Font(label.Font, FontStyle.Italic);
+                }
+            }
+        }
+
+    }
+}
