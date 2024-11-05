0f934d1ddcb897fecc3509662c71c466c8e99ff9
Daniil Stankov <stankov370@gmail.com>
2024-11-05 12:20:27 +0200

    Task_10 done.

---
+++ Lab_7/Task_10/Task_10.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_10
+{
+    public partial class Task_10 : Form
+    {
+        public Task_10()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox_Enter(object sender, EventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+
+            textBox.Font = new Font(textBox.Font, FontStyle.Italic);
+        }
+
+        private void textBox_Leave(object sender, EventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+
+            textBox.Font = new Font(textBox.Font, FontStyle.Regular);
+        }
+    }
+}
