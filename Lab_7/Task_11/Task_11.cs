11b818325a11245e2e3af43e20e48be4d5280802
Daniil Stankov <stankov370@gmail.com>
2024-11-05 13:36:06 +0200

    Task_11 done.

---
+++ Lab_7/Task_11/Task_11.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_11
+{
+    public partial class Task_11 : Form
+    {
+        public Task_11()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar < 'a' || e.KeyChar > 'm')
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
