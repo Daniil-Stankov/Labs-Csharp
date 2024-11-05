7459d2fde5588c7077dee38189376d9cef9cda31
Daniil Stankov <stankov370@gmail.com>
2024-11-05 11:09:33 +0200

    Task_6 done.

---
+++ Lab_7/Task_6/Task_6.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_6
+{
+    public partial class Task_6 : Form
+    {
+        public Task_6()
+        {
+            InitializeComponent();
+        }
+
+        private void button_MouseEnter(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+
+            button.Font = new Font(button.Font, FontStyle.Italic);
+        }
+
+        private void button_MouseLeave(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+
+            button.Font = new Font(button.Font, FontStyle.Regular);
+        }
+    }
+}
