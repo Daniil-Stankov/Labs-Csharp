09c9e55c2a10efd9ae97691cbc2f2b7008a56503
Daniil Stankov <stankov370@gmail.com>
2024-11-16 01:49:17 +0200

    Task_9-10 done.

---
+++ Lab_8/Task_9-10/Task_9-10.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task__
+{
+    public partial class Task_9 : Form
+    {
+        public Task_9()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripButton_Click(object sender, EventArgs e)
+        {
+            if (sender is ToolStripButton button)
+            {
+                string toolTipText = button.ToolTipText;
+                MessageBox.Show(toolTipText, "Підказка", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+    }
+}
