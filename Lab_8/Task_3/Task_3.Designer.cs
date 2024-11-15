c200b7ccf1e0559243b1b55f402d6be0e2c254d5
Daniil Stankov <stankov370@gmail.com>
2024-11-16 00:10:02 +0200

    Task_3 done

---
+++ Lab_8/Task_3/Task_3.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Task_5
+{
+    partial class Task_3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(367, 144);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.checkBox1.Size = new System.Drawing.Size(56, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Спорт";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(315, 162);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.checkBox2.Size = new System.Drawing.Size(120, 24);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Мандрівки";
+            this.checkBox2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.Checked = true;
+            this.checkBox3.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox3.Location = new System.Drawing.Point(315, 186);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.checkBox3.Size = new System.Drawing.Size(120, 24);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "Майстрування";
+            this.checkBox3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.Location = new System.Drawing.Point(292, 216);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.checkBox4.Size = new System.Drawing.Size(155, 17);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "Малювання";
+            this.checkBox4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(307, 230);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 41);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Вибрати";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(388, 230);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 41);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Відмовитися";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(310, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Виберіть ваші інтереси:";
+            // 
+            // Task_3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Task_3";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Lab_8/Task_3/Task_3.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace Task_5
+{
+    public partial class Task_3 : Form
+    {
+        public Task_3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+                checkBox1.Font = new Font(checkBox1.Font, FontStyle.Italic);
+            if (checkBox2.Checked)
+                checkBox2.Font = new Font(checkBox2.Font, FontStyle.Italic);
+            if (checkBox3.Checked)
+                checkBox3.Font = new Font(checkBox3.Font, FontStyle.Italic);
+            if (checkBox4.Checked)
+                checkBox4.Font = new Font(checkBox4.Font, FontStyle.Italic);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Скидаємо вибір checkBox
+            checkBox1.Checked = false;
+            checkBox2.Checked = true; // За замовчуванням
+            checkBox3.Checked = true; // За замовчуванням
+            checkBox4.Checked = false;
+
+            Reset();
+        }
+
+        private void Reset()
+        {
+            checkBox1.Font = new Font(checkBox1.Font, FontStyle.Regular);
+            checkBox2.Font = new Font(checkBox2.Font, FontStyle.Regular);
+            checkBox3.Font = new Font(checkBox3.Font, FontStyle.Regular);
+            checkBox4.Font = new Font(checkBox4.Font, FontStyle.Regular);
+        }
+    }
+}
