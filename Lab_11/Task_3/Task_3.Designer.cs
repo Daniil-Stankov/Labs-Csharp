4d0e1fb8b5f428a28c990b4263100a1c4024dafe
Daniil Stankov <stankov370@gmail.com>
2024-11-22 22:11:26 +0200

    Task_3 done

---
+++ Lab_11/Task_3/Task_3.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace Task_3
+{
+    partial class Task_3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(172, 412);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Все на одній формі";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Location = new System.Drawing.Point(190, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(189, 412);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Дані з окремої форми";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBox3);
+            this.groupBox3.Controls.Add(this.button4);
+            this.groupBox3.Location = new System.Drawing.Point(385, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(192, 412);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Результати в окрему форму";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.button6);
+            this.groupBox4.Controls.Add(this.button5);
+            this.groupBox4.Location = new System.Drawing.Point(583, 12);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(205, 412);
+            this.groupBox4.TabIndex = 1;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Все на окремих формах";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Розрахувати";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(21, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(23, 83);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Ввести дані";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 156);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Введені дані:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 178);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 26);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Периметр: \r\nПлоща: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Результат:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Введені дані:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(23, 119);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(86, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Розрахувати";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(25, 112);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(86, 36);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Вивести результат";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(46, 83);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Ввести дані";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(25, 39);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(46, 112);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(86, 36);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Вивести результат";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Task_3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Task_3";
+            this.Text = "Вирахунок периметру та площі";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Lab_11/Task_3/Task_3.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_3
+{
+    public partial class Task_3 : Form
+    {
+        private double? sideLengthSeparateForms;
+        private double? sideLength;
+
+        public Task_3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(textBox1.Text, out double side) && side > 0)
+            {
+                double perimeter = 3 * side;
+                double area = (Math.Sqrt(3) / 4) * side * side;
+                label1.Text = $"Введені дані: {side:F2}";
+                label2.Text = $"Периметр: {perimeter:F2}\nПлоща: {area:F2}";
+            }
+            else
+            {
+                MessageBox.Show("Введіть коректне значення сторони!", "Помилка");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            writeNumber writeForm = new writeNumber();
+            if (writeForm.ShowDialog() == DialogResult.OK)
+            {
+                if (double.TryParse(writeForm.InputValue, out double result) && result > 0)
+                {
+                    sideLength = result;
+                    MessageBox.Show($"Дані успішно введено: {sideLength}", "Успіх");
+                }
+                else
+                {
+                    MessageBox.Show("Введено некоректне значення. Повторіть спробу.", "Помилка");
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (sideLength.HasValue)
+            {
+                // Розрахунок периметра і площі
+                double side = sideLength.Value;
+                double perimeter = 3 * side;
+                double area = (Math.Sqrt(3) / 4) * side * side;
+
+                // Вивід результатів
+                label4.Text = $"Введені дані: {side:F2}";
+                label3.Text = $"Периметр: {perimeter:F2}\nПлоща: {area:F2}";
+            }
+            else
+            {
+                MessageBox.Show("Спершу введіть дані через форму 'Ввести дані'.", "Помилка");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(textBox3.Text, out double side) && side > 0)
+            {
+                double perimeter = 3 * side;
+                double area = (Math.Sqrt(3) / 4) * side * side;
+
+                // Створюємо нову форму showResult
+                showResult resultForm = new showResult();
+
+                // Встановлюємо текст у мітках нової форми
+                resultForm.SetData($"Введені дані: {side:F2}", $"Периметр: {perimeter:F2}, Площа: {area:F2}");
+
+                // Показуємо форму
+                resultForm.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Введіть коректне значення сторони!", "Помилка");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // Відкрити форму writeNumber для введення даних
+            writeNumber writeForm = new writeNumber();
+            if (writeForm.ShowDialog() == DialogResult.OK)
+            {
+                // Отримати введене значення
+                if (double.TryParse(writeForm.InputValue, out double result) && result > 0)
+                {
+                    sideLengthSeparateForms = result;
+                    MessageBox.Show($"Дані успішно введено: {sideLengthSeparateForms}", "Успіх");
+                }
+                else
+                {
+                    MessageBox.Show("Введено некоректне значення. Повторіть спробу.", "Помилка");
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (sideLengthSeparateForms.HasValue)
+            {
+                // Розрахувати периметр і площу
+                double side = sideLengthSeparateForms.Value;
+                double perimeter = 3 * side;
+                double area = (Math.Sqrt(3) / 4) * side * side;
+
+                // Відкрити форму showResult для відображення результатів
+                showResult resultForm = new showResult();
+                resultForm.SetData($"Введені дані: {side:F2}", $"Периметр: {perimeter:F2}, Площа: {area:F2}");
+                resultForm.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Спершу введіть дані через форму 'Ввести дані'.", "Помилка");
+            }
+        }
+    }
+}
