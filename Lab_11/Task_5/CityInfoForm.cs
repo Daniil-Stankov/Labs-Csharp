a285b49dbea4c11069c428f1864ed3a8b22df28b
Daniil Stankov <stankov370@gmail.com>
2024-11-23 02:09:57 +0200

    Task_5 done

---
+++ Lab_11/Task_5/CityInfoForm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_5
+{
+    public partial class CityInfoForm : Form
+    {
+        public CityInfoForm(string cityName)
+        {
+            InitializeComponent();
+            switch (cityName)
+            {
+                case "Делі":
+                    pictureBox1.Image = Image.FromStream(new MemoryStream(Properties.Resources.DelhiImage));
+                    label1.Text = "Делі – столиця Індії, відома своїми історичними пам'ятками.";
+                    break;
+
+                case "Мумбаї":
+                    pictureBox1.Image = Image.FromStream(new MemoryStream(Properties.Resources.MumbaiImage));
+                    label1.Text = "Мумбаї – фінансова столиця Індії.";
+                    break;
+
+                case "Ченнаї":
+                    pictureBox1.Image = Image.FromStream(new MemoryStream(Properties.Resources.ChennaiImage));
+                    label1.Text = "Ченнаї – центр культури та мистецтва півдня Індії.";
+                    break;
+
+                case "Бангалор":
+                    pictureBox1.Image = Image.FromStream(new MemoryStream(Properties.Resources.BangaloreImage));
+                    label1.Text = "Бангалор – відомий як Індійська Кремнієва долина.";
+                    break;
+
+                case "Джайпур":
+                    pictureBox1.Image = Image.FromStream(new MemoryStream(Properties.Resources.JaipurImage));
+                    label1.Text = "Джайпур – рожеве місто Індії, популярне серед туристів.";
+                    break;
+
+                default:
+                    label1.Text = "Інформація про місто недоступна.";
+                    break;
+            }
+        }
+    }
+}
---
+++ Lab_11/Task_5/Task_5.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_5
+{
+    public partial class Task_5 : Form
+    {
+        public Task_5()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem == null)
+            {
+                MessageBox.Show("Оберіть місто для відображення інформації.", "Помилка");
+                return;
+            }
+
+            string selectedCity = comboBox1.SelectedItem.ToString();
+            CityInfoForm infoForm = new CityInfoForm(selectedCity);
+            infoForm.ShowDialog();
+        }
+    }
+}
