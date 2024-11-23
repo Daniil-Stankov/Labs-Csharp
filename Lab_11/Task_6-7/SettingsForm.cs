d1d5bcce67a935ac26d92ab0a778ca9af6d41556
Daniil Stankov <stankov370@gmail.com>
2024-11-23 02:11:29 +0200

    Task_6-7 done

---
+++ Lab_11/Task_6-7/SettingsForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_6
+{
+    public partial class SettingsForm : Form
+    {
+        private Task_6 mainForm;
+
+        public SettingsForm(Task_6 form)
+        {
+            InitializeComponent();
+            mainForm = form;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                mainForm.BackColor = colorDialog.Color;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                mainForm.ForeColor = colorDialog.Color;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDialog = new FontDialog();
+            if (fontDialog.ShowDialog() == DialogResult.OK)
+            {
+                mainForm.Font = fontDialog.Font;
+            }
+        }
+    }
+}
---
+++ Lab_11/Task_6-7/Task_6.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace Task_6
+{
+    public partial class Task_6 : Form
+    {
+        public Task_6()
+        {
+            InitializeComponent();
+        }
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            // Збір даних із текстових полів
+            string name = textBox1.Text;
+            string phone = textBox2.Text;
+            string email = textBox3.Text;
+            string date = textBox4.Text;
+            string dishes = richTextBox1.Text;
+
+            // Збір даних із ComboBox
+            string age = comboBox2.SelectedItem?.ToString() ?? "Не вибрано";
+            string cuisine = comboBox1.SelectedItem?.ToString() ?? "Не вибрано";
+
+            // Збір даних із RadioButton
+            string reason = "";
+            if (radioButton1.Checked) reason = radioButton1.Text;
+            else if (radioButton2.Checked) reason = radioButton2.Text;
+            else if (radioButton3.Checked) reason = radioButton3.Text;
+            else if (radioButton4.Checked) reason = radioButton4.Text;
+
+            string recommend = radioButton5.Checked ? "Так" : radioButton6.Checked ? "Ні" : "Не вибрано";
+
+            // Формування повідомлення
+            string message = $"Ім'я: {name}\nТелефон: {phone}\nПошта: {email}\nДата відвідування: {date}\n" +
+                             $"Вік: {age}\nУлюблена кухня: {cuisine}\nСтрави: {dishes}\n" +
+                             $"Чому обрали заклад: {reason}\nРекомендуватимете друзям: {recommend}";
+
+            // Відображення повідомлення
+            MessageBox.Show(message, "Відправлені дані", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            // Очистка текстових полів
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            richTextBox1.Clear();
+
+            // Скидання ComboBox до початкового стану
+            comboBox1.SelectedIndex = -1; // Повертає стан без вибору
+            comboBox2.SelectedIndex = -1;
+
+            // Скидання RadioButton до початкового стану
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+            radioButton4.Checked = false;
+            radioButton5.Checked = false;
+            radioButton6.Checked = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SettingsForm settingsForm = new SettingsForm(this);
+            settingsForm.ShowDialog();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*",
+                Title = "Зберегти налаштування"
+            };
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter writer = new StreamWriter(saveFileDialog.FileName))
+                {
+                    // Збереження значень текстових полів
+                    writer.WriteLine(textBox1.Text);
+                    writer.WriteLine(textBox2.Text);
+                    writer.WriteLine(textBox3.Text);
+                    writer.WriteLine(textBox4.Text);
+
+                    // Збереження значень RichTextBox
+                    writer.WriteLine(richTextBox1.Text);
+
+                    // Збереження стану ComboBox
+                    writer.WriteLine(comboBox1.SelectedIndex);
+                    writer.WriteLine(comboBox2.SelectedIndex);
+
+                    // Збереження стану RadioButton
+                    writer.WriteLine(radioButton1.Checked);
+                    writer.WriteLine(radioButton2.Checked);
+                    writer.WriteLine(radioButton3.Checked);
+                    writer.WriteLine(radioButton4.Checked);
+                    writer.WriteLine(radioButton5.Checked);
+                    writer.WriteLine(radioButton6.Checked);
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*",
+                Title = "Завантажити налаштування"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamReader reader = new StreamReader(openFileDialog.FileName))
+                {
+                    // Завантаження значень текстових полів
+                    textBox1.Text = reader.ReadLine();
+                    textBox2.Text = reader.ReadLine();
+                    textBox3.Text = reader.ReadLine();
+                    textBox4.Text = reader.ReadLine();
+
+                    // Завантаження значень RichTextBox
+                    richTextBox1.Text = reader.ReadLine();
+
+                    // Завантаження стану ComboBox
+                    comboBox1.SelectedIndex = int.Parse(reader.ReadLine());
+                    comboBox2.SelectedIndex = int.Parse(reader.ReadLine());
+
+                    // Завантаження стану RadioButton
+                    radioButton1.Checked = bool.Parse(reader.ReadLine());
+                    radioButton2.Checked = bool.Parse(reader.ReadLine());
+                    radioButton3.Checked = bool.Parse(reader.ReadLine());
+                    radioButton4.Checked = bool.Parse(reader.ReadLine());
+                    radioButton5.Checked = bool.Parse(reader.ReadLine());
+                    radioButton6.Checked = bool.Parse(reader.ReadLine());
+                }
+            }
+        }
+
+        private void LoadSettingsOnStartup()
+        {
+            string configFile = "config.txt";
+
+            if (File.Exists(configFile))
+            {
+                using (StreamReader reader = new StreamReader(configFile))
+                {
+                    // Завантаження значень текстових полів
+                    textBox1.Text = reader.ReadLine();
+                    textBox2.Text = reader.ReadLine();
+                    textBox3.Text = reader.ReadLine();
+                    textBox4.Text = reader.ReadLine();
+
+                    // Завантаження значень RichTextBox
+                    richTextBox1.Text = reader.ReadLine();
+
+                    // Завантаження стану ComboBox
+                    comboBox1.SelectedIndex = int.Parse(reader.ReadLine());
+                    comboBox2.SelectedIndex = int.Parse(reader.ReadLine());
+
+                    // Завантаження стану RadioButton
+                    radioButton1.Checked = bool.Parse(reader.ReadLine());
+                    radioButton2.Checked = bool.Parse(reader.ReadLine());
+                    radioButton3.Checked = bool.Parse(reader.ReadLine());
+                    radioButton4.Checked = bool.Parse(reader.ReadLine());
+                    radioButton5.Checked = bool.Parse(reader.ReadLine());
+                    radioButton6.Checked = bool.Parse(reader.ReadLine());
+                }
+            }
+        }
+
+        private void SaveSettingsOnExit()
+        {
+            string configFile = "config.txt";
+
+            using (StreamWriter writer = new StreamWriter(configFile))
+            {
+                // Збереження значень текстових полів
+                writer.WriteLine(textBox1.Text);
+                writer.WriteLine(textBox2.Text);
+                writer.WriteLine(textBox3.Text);
+                writer.WriteLine(textBox4.Text);
+
+                // Збереження значень RichTextBox
+                writer.WriteLine(richTextBox1.Text);
+
+                // Збереження стану ComboBox
+                writer.WriteLine(comboBox1.SelectedIndex);
+                writer.WriteLine(comboBox2.SelectedIndex);
+
+                // Збереження стану RadioButton
+                writer.WriteLine(radioButton1.Checked);
+                writer.WriteLine(radioButton2.Checked);
+                writer.WriteLine(radioButton3.Checked);
+                writer.WriteLine(radioButton4.Checked);
+                writer.WriteLine(radioButton5.Checked);
+                writer.WriteLine(radioButton6.Checked);
+            }
+        }
+
+
+        private void Task_6_Load(object sender, EventArgs e)
+        {
+            LoadSettingsOnStartup();
+        }
+
+        private void Task_6_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveSettingsOnExit();
+        }
+    }
+}
