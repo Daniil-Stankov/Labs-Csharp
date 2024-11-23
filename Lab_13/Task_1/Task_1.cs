ceacbd707b57305310d2d0232da1de51f3fd215b
Daniil Stankov <stankov370@gmail.com>
2024-11-23 22:52:03 +0200

    Task_1 done

---
+++ Lab_13/Task_1/Task_1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab13
+{
+    public partial class Task_1 : Form
+    {
+        public Task_1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            // Отримуємо графіку для PictureBox
+            Graphics g = e.Graphics;
+
+            int width = pictureBox1.Width;
+            int height = pictureBox1.Height;
+            int stripeHeight = height / 2;
+
+            // Малюємо синю
+            g.FillRectangle(Brushes.Blue, 0, 0, width, stripeHeight);
+            // Малюємо жовту
+            g.FillRectangle(Brushes.Yellow, 0, stripeHeight, width, stripeHeight);
+            // Малюємо контур 
+            g.DrawRectangle(Pens.Black, 0, 0, width - 1, height - 1);
+        }
+    }
+}
