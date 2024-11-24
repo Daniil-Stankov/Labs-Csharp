04fd8e2acfcddfb8238865cb6f2625e40d2c77d4
Daniil Stankov <stankov370@gmail.com>
2024-11-24 21:49:03 +0200

    Task_4 done

---
+++ Lab_13/Task_4/Task_4.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Lab13
+{
+    partial class Task_4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(153, 376);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(197, 73);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(153, 297);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(197, 73);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            this.pictureBox2.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox2_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Task_4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Task_4";
+            this.Text = "Task_4";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Lab_13/Task_4/Task_4.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.AxHost;
+
+namespace Lab13
+{
+    public partial class Task_4 : Form
+    {
+        private int state = 0;
+
+        public Task_4()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            // Отримуємо графіку для PictureBox
+            Graphics g = e.Graphics;
+
+            int width = pictureBox1.Width;
+            int height = pictureBox1.Height;
+            int stripeHeight = height / 2;
+
+            // Малюємо синю
+            g.FillRectangle(Brushes.Blue, 0, 0, width, stripeHeight);
+            // Малюємо жовту
+            g.FillRectangle(Brushes.Yellow, 0, stripeHeight, width, stripeHeight);
+            // Малюємо контур 
+            g.DrawRectangle(Pens.Black, 0, 0, width - 1, height - 1);
+        }
+
+        private void pictureBox2_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Rectangle rect = pictureBox1.ClientRectangle;
+
+            int width = rect.Width;
+            int height = rect.Height;
+
+            Brush red = Brushes.Red;
+            Brush blue = Brushes.Blue;
+            Brush white = Brushes.White;
+
+            int whiteThickness = width / 8;
+            int blueThickness = whiteThickness / 2;
+
+            int verticalShift = height / 15;
+            int horizontalShift = width / -8;
+
+            g.FillRectangle(red, 0, 0, width, height);
+            g.FillRectangle(white, 0, height / 2 - whiteThickness / 2 + verticalShift, width, whiteThickness);
+            g.FillRectangle(white, width / 2 - whiteThickness / 2 + horizontalShift, 0, whiteThickness, height);
+            g.FillRectangle(blue, 0, height / 2 - blueThickness / 2 + verticalShift, width, blueThickness);
+            g.FillRectangle(blue, width / 2 - blueThickness / 2 + horizontalShift, 0, blueThickness, height);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            switch (state)
+            {
+                case 0:
+                    // Стан 0: тільки прапор України
+                    pictureBox1.Visible = true;
+                    pictureBox2.Visible = false;
+                    break;
+
+                case 1:
+                    // Стан 1: прапор норвегії поруч (праворуч)
+                    pictureBox1.Visible = true;
+                    pictureBox2.Visible = true;
+                    pictureBox2.Location = new Point(356, 376);
+                    break;
+
+                case 2:
+                    // Стан 2: прапор норвегії зверху
+                    pictureBox1.Visible = true;
+                    pictureBox2.Visible = true;
+                    pictureBox2.Location = new Point(153, 297);
+                    break;
+            }
+
+            // Перехід до наступного стану
+            state = (state + 1) % 3;
+        }
+    }
+}
