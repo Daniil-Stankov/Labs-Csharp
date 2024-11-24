890e008a37a3e95d138e5365e5c6dce0a80e3e70
Daniil Stankov <stankov370@gmail.com>
2024-11-24 21:48:47 +0200

    Task_2 done

---
+++ Lab_13/Task_2/Task_2.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab13
+{
+    public partial class Task_2 : Form
+    {
+        public Task_2()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            // Отримуємо графіку для PictureBox
+            Graphics g = e.Graphics;
+
+            int width = pictureBox1.Width;
+            int height = pictureBox1.Height;
+            int stripeHeight = height / 2;
+
+            // Малюємо синю
+            g.FillRectangle(Brushes.Blue, 0, 0, width, stripeHeight);
+            // Малюємо жовту
+            g.FillRectangle(Brushes.Yellow, 0, stripeHeight, width, stripeHeight);
+            // Малюємо контур 
+            g.DrawRectangle(Pens.Black, 0, 0, width - 1, height - 1);
+        }
+
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = true;
+            pictureBox2.Refresh(); // Оновлення прапора
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = false;
+        }
+
+        private void pictureBox2_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Rectangle rect = pictureBox1.ClientRectangle;
+
+            int width = rect.Width;
+            int height = rect.Height;
+
+            Brush red = Brushes.Red;
+            Brush blue = Brushes.Blue;
+            Brush white = Brushes.White;
+
+            int whiteThickness = width / 8;
+            int blueThickness = whiteThickness / 2;
+
+            int verticalShift = height / 15;
+            int horizontalShift = width / -8;
+
+            g.FillRectangle(red, 0, 0, width, height);
+            g.FillRectangle(white, 0, height / 2 - whiteThickness / 2 + verticalShift, width, whiteThickness);
+            g.FillRectangle(white, width / 2 - whiteThickness / 2 + horizontalShift, 0, whiteThickness, height);
+            g.FillRectangle(blue, 0, height / 2 - blueThickness / 2 + verticalShift, width, blueThickness);
+            g.FillRectangle(blue, width / 2 - blueThickness / 2 + horizontalShift, 0, blueThickness, height);
+        }
+    }
+}
