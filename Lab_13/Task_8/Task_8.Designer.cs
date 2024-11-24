de076e30b2bd9173487954931c662a56e38ce337
Daniil Stankov <stankov370@gmail.com>
2024-11-24 21:49:36 +0200

    Task_8 done

---
+++ Lab_13/Task_8/Task_8.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace Task_8
+{
+    partial class Task_8
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Task_8
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Task_8";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Task_8_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Lab_13/Task_8/Task_8.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_8
+{
+    public partial class Task_8 : Form
+    {
+        public Task_8()
+        {
+            InitializeComponent();
+        }
+
+        private void Task_8_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            float[] percentages = { 5, 25, 30, 5, 20, 15 };
+            string[] labels = { "Туніс", "Єгипет", "Туреччина", "Марокко", "Яремче", "Моршин" };
+            Color[] colors = { Color.Yellow, Color.Blue, Color.Green, Color.Orange, Color.Red, Color.Purple };
+
+            // Параметри кругової діаграми
+            Rectangle chartArea = new Rectangle(50, 50, 200, 200);
+            float startAngle = 0;
+
+            for (int i = 0; i < percentages.Length; i++)
+            {
+                float sweepAngle = percentages[i] * 360 / 100; // Розрахунок кута сектору
+                using (Brush brush = new SolidBrush(colors[i]))
+                {
+                    g.FillPie(brush, chartArea, startAngle, sweepAngle);
+                }
+                startAngle += sweepAngle;
+            }
+
+            int legendX = 270;
+            int legendY = 50;
+            for (int i = 0; i < labels.Length; i++)
+            {
+                using (Brush brush = new SolidBrush(colors[i]))
+                {
+                    g.FillRectangle(brush, legendX, legendY + i * 20, 15, 15);
+                }
+
+                g.DrawString($"{labels[i]} - {percentages[i]}%", this.Font, Brushes.Black, legendX + 20, legendY + i * 20);
+            }
+        }
+    }
+}
