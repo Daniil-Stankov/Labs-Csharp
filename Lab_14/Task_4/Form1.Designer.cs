2520395d8247921fa0223b013d40db867bb383b5
Daniil Stankov <stankov370@gmail.com>
2024-11-25 15:12:09 +0200

    Task_4 done

---
+++ Lab_14/Task_4/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Task_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(442, 202);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(66, 17);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Сховати";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(442, 178);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(74, 17);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Показати";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton_CheckedChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(232, 178);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(172, 21);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 178);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 6;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 150);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.ColumnHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.DataGridView1_ColumnHeaderMouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Lab_14/Task_4/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySqlConnector;
+
+namespace Task_4
+{
+    public partial class Form1 : Form
+    {
+        string constring = "server=localhost;uid=root;pwd=1234;database=landmanagement";
+        DataTable currentTable = new DataTable();
+        private bool sortAscending = true;  // Для визначення напрямку сортування
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadTables();
+        }
+
+        private void LoadTables()
+        {
+            // Додаємо назви таблиць до ComboBox
+            comboBox1.Items.Add("Підприємство");
+            comboBox1.Items.Add("ЗемельнаДілянка");
+            comboBox1.Items.Add("ЗемлеВикористання");
+            comboBox1.SelectedIndex = 0;  // Встановлюємо початковий вибір
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadData(comboBox1.SelectedItem.ToString());
+        }
+
+        private void LoadData(string tableName)
+        {
+            using (MySqlConnection con = new MySqlConnection(constring))
+            {
+                try
+                {
+                    con.Open();
+                    string sql = $"SELECT * FROM {tableName}";
+                    MySqlDataAdapter adapter = new MySqlDataAdapter(sql, con);
+                    currentTable = new DataTable();
+                    adapter.Fill(currentTable);
+
+                    // Відображення даних у DataGridView
+                    dataGridView1.DataSource = currentTable;
+
+                    // Налаштування події для сортування по кліку на заголовок стовпця
+                    dataGridView1.ColumnHeaderMouseClick += DataGridView1_ColumnHeaderMouseClick;
+
+                    // Перемикання між режимами
+                    if (radioButton1.Checked)
+                    {
+                        dataGridView1.Visible = true;
+                        panel1.Visible = false;
+                    }
+                    else
+                    {
+                        DisplayFormular();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Помилка: " + ex.Message);
+                }
+            }
+        }
+
+        // Сортування даних при кліку на заголовок стовпця
+        private void DataGridView1_ColumnHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            string columnName = dataGridView1.Columns[e.ColumnIndex].Name;
+            SortData(columnName);
+        }
+
+        // Сортування даних по вибраному стовпцю
+        private void SortData(string columnName)
+        {
+            if (sortAscending)
+            {
+                currentTable.DefaultView.Sort = $"{columnName} ASC";
+            }
+            else
+            {
+                currentTable.DefaultView.Sort = $"{columnName} DESC";
+            }
+            sortAscending = !sortAscending;  // Перемикаємо напрямок сортування
+        }
+
+        // Відображення даних у вигляді форми
+        private void DisplayFormular()
+        {
+            panel1.Controls.Clear();
+            dataGridView1.Visible = false;
+            panel1.Visible = true;
+
+            int yPosition = 10;
+            foreach (DataRow row in currentTable.Rows)
+            {
+                foreach (DataColumn col in currentTable.Columns)
+                {
+                    Label lbl = new Label
+                    {
+                        Text = $"{col.ColumnName}: {row[col]}",
+                        Location = new Point(10, yPosition),
+                        AutoSize = true
+                    };
+                    panel1.Controls.Add(lbl);
+                    yPosition += 30;
+                }
+                yPosition += 20; // Відстань між об'єктами
+            }
+        }
+
+        // Перемикання між режимами (Таблиця або Формуляр)
+        private void radioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                LoadData(comboBox1.SelectedItem.ToString());
+            }
+        }
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem == null)
+            {
+                LoadData(comboBox1.SelectedItem.ToString());
+            }
+        }
+    }
+}
