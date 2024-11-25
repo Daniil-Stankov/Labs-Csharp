9345ccb7c45eceab6adeeeee14e93c9413a4e5e5
Daniil Stankov <stankov370@gmail.com>
2024-11-25 15:12:24 +0200

    Task_6 done

---
+++ Lab_14/Task_6/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Task_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(442, 202);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(66, 17);
+            this.radioButton2.TabIndex = 14;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Сховати";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.RadioButton_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(442, 178);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(74, 17);
+            this.radioButton1.TabIndex = 13;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Показати";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.RadioButton_CheckedChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(232, 178);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(172, 21);
+            this.comboBox1.TabIndex = 12;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 178);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 11;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 150);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(108, 303);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 15;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(214, 303);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 16;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(232, 329);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Edit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(313, 329);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Add";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(394, 329);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(321, 302);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 20;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(427, 303);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 21;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(534, 302);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 22;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ Lab_14/Task_6/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySqlConnector;
+
+namespace Task_6
+{
+    public partial class Form1 : Form
+    {
+        string constring = "server=localhost;uid=root;pwd=1234;database=LandManagement";
+        DataTable currentTable = new DataTable();
+        string selectedTable;
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadTables();
+        }
+
+        // Завантажуємо назви таблиць у ComboBox
+        private void LoadTables()
+        {
+            comboBox1.Items.Add("Підприємство");
+            comboBox1.Items.Add("ЗемельнаДілянка");
+            comboBox1.Items.Add("ЗемлеВикористання");
+            comboBox1.SelectedIndex = 0;
+        }
+
+        // Завантаження даних з вибраної таблиці
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedTable = comboBox1.SelectedItem.ToString();
+            LoadData(selectedTable);
+        }
+
+        // Завантаження даних у DataGridView
+        private void LoadData(string tableName)
+        {
+            using (MySqlConnection con = new MySqlConnection(constring))
+            {
+                try
+                {
+                    con.Open();
+                    string sql = $"SELECT * FROM `{tableName}`";
+                    MySqlDataAdapter adapter = new MySqlDataAdapter(sql, con);
+                    currentTable = new DataTable();
+                    adapter.Fill(currentTable);
+
+                    dataGridView1.DataSource = currentTable;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Помилка: " + ex.Message);
+                }
+            }
+        }
+
+        // Додавання нового запису
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection con = new MySqlConnection(constring))
+            {
+                try
+                {
+                    con.Open();
+                    string sql = "";
+
+                    if (selectedTable == "Підприємство")
+                    {
+                        sql = $"INSERT INTO Підприємство (найменування, розрахунковий_рахунок, річний_дохід) " +
+                              $"VALUES ('{textBox1.Text}', '{textBox2.Text}', '{textBox3.Text}')";
+                    }
+                    else if (selectedTable == "ЗемельнаДілянка")
+                    {
+                        sql = $"INSERT INTO ЗемельнаДілянка (шифр_ділянки, площа, вартість, вид_грунту) " +
+                              $"VALUES ('{textBox1.Text}', '{textBox2.Text}', '{textBox3.Text}', '{textBox4.Text}')";
+                    }
+                    else if (selectedTable == "ЗемлеВикористання")
+                    {
+                        sql = $"INSERT INTO ЗемлеВикористання (підприємство_id, земельна_ділянка_id, вид_використання, термін_від, термін_до) " +
+                        $"VALUES ('{textBox1.Text}', '{textBox2.Text}', '{textBox3.Text}', '{textBox4.Text}', '{textBox5.Text}')";
+                    }
+
+                    MySqlCommand cmd = new MySqlCommand(sql, con);
+                    cmd.ExecuteNonQuery();
+                    LoadData(selectedTable);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Помилка: " + ex.Message);
+                }
+            }
+        }
+
+        // Видалення вибраного запису
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection con = new MySqlConnection(constring))
+            {
+                try
+                {
+                    con.Open();
+                    DataGridViewRow row = dataGridView1.CurrentRow;
+                    string sql = $"DELETE FROM `{selectedTable}` WHERE id = {row.Cells["id"].Value}";
+                    MySqlCommand cmd = new MySqlCommand(sql, con);
+                    cmd.ExecuteNonQuery();
+                    LoadData(selectedTable);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Помилка: " + ex.Message);
+                }
+            }
+        }
+
+        // Редагування вибраного запису
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection con = new MySqlConnection(constring))
+            {
+                try
+                {
+                    con.Open();
+                    DataGridViewRow row = dataGridView1.CurrentRow;
+                    string sql = "";
+
+                    if (selectedTable == "Підприємство")
+                    {
+                        sql = $"UPDATE Підприємство SET найменування = '{textBox1.Text}', розрахунковий_рахунок = '{textBox2.Text}', річний_дохід = '{textBox3.Text}' WHERE id = {row.Cells["id"].Value}";
+                    }
+                    else if (selectedTable == "ЗемельнаДілянка")
+                    {
+                        sql = $"UPDATE ЗемельнаДілянка SET шифр_ділянки = '{textBox1.Text}', площа = '{textBox2.Text}', вартість = '{textBox3.Text}', вид_грунту = '{textBox4.Text}' WHERE id = {row.Cells["id"].Value}";
+                    }
+                    else if (selectedTable == "ЗемлеВикористання")
+                    {
+                        sql = $"UPDATE ЗемлеВикористання SET підприємство_id = '{textBox1.Text}', земельна_ділянка_id = '{textBox2.Text}', вид_використання = '{textBox3.Text}', термін_від = '{textBox4.Text}', термін_до = '{textBox5.Text}' WHERE id = {row.Cells["id"].Value}";
+                    }
+
+                    MySqlCommand cmd = new MySqlCommand(sql, con);
+                    cmd.ExecuteNonQuery();
+                    LoadData(selectedTable);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Помилка: " + ex.Message);
+                }
+            }
+        }
+
+        private void RadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            // Перевіряємо, яка кнопка активна
+            if (radioButton1.Checked)
+            {
+                // Показуємо DataGridView і завантажуємо дані
+                dataGridView1.Visible = true;
+                if (!string.IsNullOrEmpty(selectedTable))
+                {
+                    LoadData(selectedTable); // Завантаження даних, якщо таблиця вибрана
+                }
+            }
+            else if (radioButton2.Checked)
+            {
+                // Приховуємо DataGridView
+                dataGridView1.Visible = false;
+            }
+        }
+    }
+}
